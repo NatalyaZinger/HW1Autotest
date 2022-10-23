76d713a2ae402776d13546701eaf8b236842d295
NatalyaZinger <nata.zinger@gmail.com>
2022-10-23 18:56:21 +0500

    junit4 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,37 +1,37 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+
+    @Test
     public void testRemain() {
         int amount = 400;
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int actual = cashbackHackService.remain(amount);
         int expected = 600;
-        assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void testRemainMore1000() {
         int amount = 1200;
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int actual = cashbackHackService.remain(amount);
         int expected = 800;
-        assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void testRemain1000() {
         int amount = 1000;
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
     }
-
-
-
 
 }
